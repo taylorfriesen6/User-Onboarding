16b76240ca65b305a38af869715bbc961f952666
Taylor Friesen <taylorfriesen@protonmail.com>
2021-02-24 14:27:34 -0800

    controlled form elements set up

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,6 +1,35 @@
+import {useState} from 'react';
+
 function Form(props) {
+
+  const initialUser = {
+    name: '',
+    email: '',
+    password: '',
+    termsOfService: false,
+  };
+  const [form, setForm] = useState(initialUser);
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value});
+  }
+
   return (
-    <div>test</div>
+    <form>
+      <label>
+        Name: <input type='text' name='name' value={form.name} onChange={handleChange}/>
+      </label>
+      <label>
+        Email: <input type='text' name='email' value={form.email} onChange={handleChange}/>
+      </label>
+      <label>
+        Password: <input type='password' name='password' value={form.password} onChange={handleChange}/>
+      </label>
+      <label>
+        I agree to the terms of service: <input type='checkbox' name='termsOfService' value={form.termsOfService} onChange={handleChange}/>
+      </label>
+    </form>
   );
 }
+
 export default Form;
