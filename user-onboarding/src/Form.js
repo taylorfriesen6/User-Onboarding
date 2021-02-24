24a702079cd6918d8c75d2ef957c4d1488e1bd33
Taylor Friesen <taylorfriesen@protonmail.com>
2021-02-24 15:26:17 -0800

    form validation handled

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -24,26 +24,53 @@ function Form(props) {
   };
   const [form, setForm] = useState(initialForm);
 
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    password: '',
+    termsOfService: '',
+  });
+
   const handleChange = (e) => {
     const { name, type, value, checked } = e.target;
-    const updatedInfo = type === 'checkbox' ? checked : value;
+    const updatedInfo = (type === 'checkbox') ? checked : value;
     setForm({ ...form, [name]: updatedInfo });
+    yup.reach(schema, name).validate(value).then(_ => {
+      setErrors({...errors, [name]: ''});
+    }).catch(err => {
+      setErrors({...errors, [name]: err.errors[0]})
+    });
   }
 
   return (
     <form>
-      <label>
-        Name: <input type='text' name='name' value={form.name} onChange={handleChange}/>
-      </label>
-      <label>
-        Email: <input type='text' name='email' value={form.email} onChange={handleChange}/>
-      </label>
-      <label>
-        Password: <input type='password' name='password' value={form.password} onChange={handleChange}/>
-      </label>
-      <label>
-        I agree to the terms of service: <input type='checkbox' name='termsOfService' checked={form.termsOfService} onChange={handleChange}/>
-      </label>
+      <p>
+        <label>
+          Name: <input type='text' name='name' value={form.name} onChange={handleChange}/>
+        </label>
+        <span className="error">{errors.name}</span>
+      </p>
+      <p>
+        <label>
+          Email: <input type='text' name='email' value={form.email} onChange={handleChange}/>
+        </label>
+        <span className="error">{errors.email}</span>
+      </p>
+      <p>
+        <label>
+          Password: <input type='password' name='password' value={form.password} onChange={handleChange}/>
+        </label>
+        <span className="error">{errors.password}</span>
+      </p>
+      <p>
+        <label>
+          I agree to the terms of service: <input type='checkbox' name='termsOfService' checked={form.termsOfService} onChange={handleChange}/>
+        </label>
+        <span className="error">{errors.termsOfService}</span>
+      </p>
+      <p>
+        <input type='submit'/>
+      </p>
     </form>
   );
 }
