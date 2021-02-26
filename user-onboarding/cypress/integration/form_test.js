583974c5d33228998e59f9e75ac0407fe5ce43a6
Taylor Friesen <taylorfriesen@protonmail.com>
2021-02-25 19:46:46 -0800

    tests for text fields

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -1,6 +1,18 @@
 describe('Form', () => {
   it('receives input in name field', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('input[name=name]').type('qqq');
+    cy.get('input[name=name]').type('Taylor')
+      .should('have.value', 'Taylor');
   })
+  it('receives input in email field', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('input[name=email]').type('taylorfriesen@protonmail.com')
+      .should('have.value', 'taylorfriesen@protonmail.com');
+  })
+  it('receives input in password field', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('input[name=password]').type('swordfish')
+      .should('have.value', 'swordfish');
+  })
+
 })
